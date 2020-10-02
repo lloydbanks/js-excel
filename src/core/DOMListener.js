d16c851f5cfdcf98734af7f05628b29535c5554d
Dmitry <dmitry.litvinov.spb@gmail.com>
2020-10-03 00:45:17 +0300

    32: add template & prettier

--- src/core/DOMListener.js
+++ src/core/DOMListener.js
@@ -1,3 +1,7 @@
 export default class DOMListener {
-
+  constructor($root) {
+    if (!$root) throw new Error('Parent element is required for DOMListener!')
+    this.$root = $root
+    console.log('dom root', this.$root)
+  }
 }
