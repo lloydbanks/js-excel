8902d4f43c4aac1df883524ce19adb895491721a
Dmitry <dmitry.litvinov.spb@gmail.com>
2020-10-04 02:27:36 +0300

    33: add DOM class methods

--- src/core/DOMListener.js
+++ src/core/DOMListener.js
@@ -1,7 +1,9 @@
 export default class DOMListener {
   constructor($root) {
-    if (!$root) throw new Error('Parent element is required for DOMListener!')
-    this.$root = $root
+    if (!$root && $root.$el) {
+      throw new Error('Parent element is required for DOMListener!')
+    }
+    this.$root = $root.$el
     console.log('dom root', this.$root)
   }
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -1,14 +1,42 @@
 class DOM {
-  constructor() {
+  constructor(selector) {
+    this.$el =
+      typeof selector === 'string' ? document.querySelector(selector) : selector
+  }
+
+  html(html) {
+    const { $el } = this
+    if (typeof html === 'string') {
+      $el.innerHTML = html
+      return this
+    }
+
+    return $el.outerHTML.trim()
+  }
+
+  append(node) {
+    if (node instanceof DOM) node = node.$el
+
+    if (Element.prototype.append) {
+      this.$el.append(node)
+    } else {
+      this.$el.appendChild(node)
+    }
+    return this
+  }
+
+  clear() {
+    this.html('')
+    return this
   }
 }
 
-export function $() {
-  return new DOM()
+export function $(selector) {
+  return new DOM(selector)
 }
 
 $.create = (tagName, classes = '') => {
   const el = document.createElement(tagName)
   if (classes) el.classList.add(classes)
-  return el
+  return $(el)
 }
