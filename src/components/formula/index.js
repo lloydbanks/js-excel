a6dd2fbe9f005aaec8fa3287b465e80f888156a1
Dmitry <dmitry.litvinov.spb@gmail.com>
2020-10-05 23:39:23 +0300

    34: add events structure

--- src/components/formula/index.js
+++ src/components/formula/index.js
@@ -1,11 +1,23 @@
-import { ExcelComponent } from '@core/ExcelComponent';
+import { ExcelComponent } from '@core/ExcelComponent'
 
 export default class Formula extends ExcelComponent {
+  constructor($root, options) {
+    super($root, {
+      name: 'Formula',
+      listeners: ['input'],
+    })
+  }
+
   static className = 'excel__formula'
+
   toHTML() {
     return `
       <div class="info">fx</div>
       <div class="input" contenteditable spellcheck="false"></div>
     `
+  }
+
+  onInput(e) {
+    console.log('formula onInput...', e)
   }
 }
--- src/core/DOMListener.js
+++ src/core/DOMListener.js
@@ -1,9 +1,14 @@
 export default class DOMListener {
-  constructor($root) {
+  constructor($root, listeners = []) {
     if (!$root && $root.$el) {
       throw new Error('Parent element is required for DOMListener!')
     }
     this.$root = $root.$el
-    console.log('dom root', this.$root)
+    this.listeners = listeners
   }
+
+  initDOMListeners() {
+    console.log('listeners', this.listeners)
+  }
+  removeDOMListeners() {}
 }
