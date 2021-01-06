76912c8e61e8ee7d6e8d09375ee43aab05174faf
Dmitry <dmitry.litvinov.spb@gmail.com>
2021-01-07 00:09:37 +0300

    42: add row & column resizer

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -2,6 +2,11 @@ import { ExcelComponent } from '@core/ExcelComponent'
 import { createTable } from '@/components/table/template'
 
 export default class Table extends ExcelComponent {
+  constructor($root) {
+    super($root, {
+      // listeners: ['click', 'mousedown', 'mousemove', 'mouseup'],
+    })
+  }
   static className = 'excel__table'
   toHTML() {
     return createTable()
--- src/components/table/template.js
+++ src/components/table/template.js
@@ -4,13 +4,23 @@ import { fillArray, toChar } from '@core/utils'
 const { A, Z } = CHAR_CODES
 
 const createRow = (data, i = '') => {
+  const resizer = i ? '<div class="row-resize"></div>' : ''
+
   return `
     <div class="row">
-        <div class="row-info">${i}</div>
+        <div class="row-info">
+            ${i}
+            ${resizer}
+        </div>
         <div class="row-data">${data}</div>
     </div>`
 }
-const createColumn = v => `<div class="column">${v}</div>`
+const createColumn = v => `
+    <div class="column">
+        ${v}
+        <div class="column-resize"></div>
+    </div>
+`
 const createCharColumn = (_, i) => createColumn(toChar(i + A))
 const createCell = (v = '') => `<div class="cell" contenteditable>${v}</div>`
 const createCellColumn = () => createCell()
